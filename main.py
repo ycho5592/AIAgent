1610834e7f03dbc64c804efda3036e71182e65a5
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-01-20 21:30:35 -0500

    Adding gemini

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("Environment Variable wasn't found")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(model="gemini-3-flash-preview", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+print(response.text)
+
 def main():
     print("Hello from aiagent!")
 
